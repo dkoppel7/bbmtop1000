ef48450d65019a53549f9d63a54df392e1b047fb
Daniel Koppel <104925543+dkoppel7@users.noreply.github.com>
2023-10-10 10:44:45 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,19 +5,21 @@ if __name__ == '__main__':
 
     @st.cache_data
     def get_ownership():
-        ownership = pd.read_csv('https://bbmtop1000.s3.us-east-2.amazonaws.com/ownership.csv')
+        ownership = pd.read_csv('https://bbmw5.s3.us-east-2.amazonaws.com/ownership.csv')
+        ownership = ownership.drop('Unnamed: 0', axis=1)
         return ownership
 
-    ownership = pd.read_csv('https://bbmtop1000.s3.us-east-2.amazonaws.com/ownership.csv')
-    st.write('Player Ownership in Top 1000 Teams')
+    ownership = get_ownership()
+    st.write('Player Ownership in Top 10000 Teams')
     ownership
 
 
     @st.cache_data
     def get_teams():
-        teams_data = pd.read_csv('https://bbmtop1000.s3.us-east-2.amazonaws.com/teams_data.csv')
+        teams_data = pd.read_csv('https://bbmw5.s3.us-east-2.amazonaws.com/teams_data.csv')
+        teams_data = teams_data.drop('Unnamed: 0', axis=1)
         return teams_data
 
     teams_data = get_teams()
-    st.write('Top 1000 Teams')
+    st.write('Top 10000 Teams')
     teams_data
